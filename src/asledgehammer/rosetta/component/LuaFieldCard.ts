0205889fd52f1b89c1727547bbb4e19b34d16bf9
JabDoesThings <JabDoesThings@github.com>
2024-04-20 18:47:46 -0500

    Migrated `listenType` and `renderType` to `LuaClass`.

--- src/asledgehammer/rosetta/component/LuaFieldCard.ts
+++ src/asledgehammer/rosetta/component/LuaFieldCard.ts
@@ -49,7 +49,7 @@ export class LuaFieldCard extends LuaCard<LuaFieldCardOptions> {
                 ${this.renderType(entity.name, entity.type, idType)}
 
                 <hr>
-                
+
                 ${this.renderPreview(false)}
             </div>
         `;
@@ -63,67 +63,6 @@ export class LuaFieldCard extends LuaCard<LuaFieldCardOptions> {
 
         this.listenNotes(entity, idNotes);
         this.listenType(entity, idType, idType);
-    }
-
-    listenType(entity: { name: string, type: string }, idType: string, idSelect: string): void {
-
-        const $select = $get(idType);
-        const $customInput = $get(`${idSelect}-custom-input`);
-        $select.on('change', (value) => {
-            entity.type = value.target.value;
-            if (entity.type === 'custom') {
-                $customInput.show();
-            } else {
-                $customInput.hide();
-                $customInput.val(''); // Clear custom field.
-            }
-            this.app.renderCode();
-        });
-
-        $customInput.on('focusout', () => {
-            const value = $customInput.val().trim();
-            switch (value.toLowerCase()) {
-                // Here the reference stays valid.
-                case 'custom':
-                    break;
-                // Here the reference converts to its select option.
-                case 'void':
-                case 'any':
-                case 'nil':
-                case 'boolean':
-                case 'number':
-                case 'string':
-                    entity.type = value;
-                    $select.val(value);
-                    $customInput.hide();
-                    $customInput.val(''); // Clear custom field.
-                    this.app.renderCode();
-                    break;
-            }
-        });
-    }
-
-    renderType(name: string, type: string, idReturnType: string): string {
-
-        const idTypeCard = `${name}-type-card`;
-
-        return html`
-            <div class="card border border-1 border-800 rounded-0 bg-secondary">
-                <div class="card-header bg-dark" style="cursor: pointer; background-color: var(--bs-accordion-btn-bg);">
-                    <!-- <h6 class="mb-0">Returns</h6> -->
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#${idTypeCard}" aria-expanded="true" aria-controls="${idTypeCard}">
-                        Returns
-                    </button>   
-                </div>
-                <div id="${idTypeCard}" class="card-body collapse show">
-                    <!-- Return Type -->
-                    <div>
-                        <label for="${idReturnType}" class="form-label">Type</label>
-                        ${LuaCard.renderTypeSelect(idReturnType, 'The return type.', type, false)}
-                    </div>
-                </div>
-            </div>
-        `;
     }
 }
 
